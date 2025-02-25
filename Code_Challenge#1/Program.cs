9c8f5195e1833357a5a4b25cf27401379accfa3e
thiagcarvalho <thiagoc413@gmail.com>
2025-02-24 21:50:33 -0300

    add first code challenge

---
+++ Code_Challenge#1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_1, number_2, total;
+
+            Console.WriteLine("Enter the first number: ");
+            number_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            number_2 = Convert.ToInt32(Console.ReadLine());
+
+            total = number_1 + number_2;
+            Console.WriteLine("The sum between " + number_1 + " and " + number_2 + " is: " + total);
+
+
+        }
+    }
+}
