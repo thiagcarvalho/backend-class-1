d61de3fc7bf3468d54a2b634cdae6466cbdc38a7
thiagcarvalho <thiagoc413@gmail.com>
2025-03-10 11:23:52 -0300

    add class7 code challenge

---
+++ class7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new CustomException("This is an example of custom exception.");
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine($"Caught custom exception: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Caught general exception: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block was executed.");
+            }
+        }
+    }
+
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ class7/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("class7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("class7")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d619fb8c-9153-4924-845b-41d12af73e31")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
