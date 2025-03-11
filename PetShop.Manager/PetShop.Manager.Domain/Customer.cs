edbffcecdeab2f819c0044c6f046f2603206d482
thiagcarvalho <thiagoc413@gmail.com>
2025-03-11 20:28:52 -0300

    create the files for the PetShop Manager

---
+++ PetShop.Manager/PetShop.Manager.Domain/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Manager.Domain
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<Pet> Pets { get; set; } = new List<Pet>();
+
+        public Customer(string name, string email, string phone){
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+
+        public void AddPet(Pet pet){
+            Pets.Add(pet);
+        }
+
+    }
+}
---
+++ PetShop.Manager/PetShop.Manager.Domain/IAnimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Manager.Domain
+{
+    public interface IAnimal
+    {
+        string Type { get; }
+
+        void MakeSound();
+    }
+}
