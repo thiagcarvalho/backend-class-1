23e4f500f48575513be97d0867f60b9c57f9ff80
thiagcarvalho <thiagoc413@gmail.com>
2025-03-17 15:25:04 -0300

    add dto and pet controller for middleware tests

---
+++ PetShop.Manager/Petshop.Manager.WebApi/Controllers/PetsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petshop.Manager.WebApi.Dto;
+
+
+namespace Petshop.Manager.WebApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PetsController : ControllerBase
+    {
+        public static Dictionary<string, PetDto> Pets { get; set; } = [];
+
+        [HttpPost]
+        public IActionResult Post(PetDto pet)
+        {
+            Pets.Add(pet.Name, pet);
+            return Created();
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult Get(string name){
+            if (Pets.ContainsKey(name)){
+                return Ok(Pets[name]);
+            }
+
+            return NotFound();
+
+        }
+    }
+}
