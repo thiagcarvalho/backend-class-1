9ac5d306f55c03c938813b92f47c361df6a4f54c
Thiago Carvalho <thiagoc413@gmail.com>
2025-05-09 16:20:12 -0300

    add class 2 exercise

---
+++ backend-II/class_2/class_2/class_2/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AttributeDescription;
+
+namespace TeamClass
+{
+    public class Team
+    {
+        [SpecialDescription("This is the team name")]
+        public string Name { get; set; } = string.Empty;
+
+        [SpecialDescription("This is the Team city")]
+        public string City { get; set; } = string.Empty;
+
+        [SpecialDescription("This is the Team players")]
+        public List<string> Players { get; set; } = [];
+    }
+}
